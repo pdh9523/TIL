54cb59a7a856bce1433d9403cd2573929f00facb
박동현 <jinee9523@gmail.com>
2026-02-02 14:34:39 +0900

    refactor: publish message json to string

--- pubsub.go
+++ pubsub.go
@@ -2,13 +2,12 @@ package cache
 
 import (
 	"context"
-	"encoding/json"
-	"log"
+	"fmt"
 )
 
 type Message struct {
-	SenderID string `json:"sender_id"`
-	Key      string `json:"key"`
+	SenderID string
+	Key      string
 }
 
 func (c *Cache) startSubscriber(ctx context.Context) {
@@ -19,11 +18,7 @@ func (c *Cache) startSubscriber(ctx context.Context) {
 		case <-ctx.Done():
 			return
 		case msg := <-ch:
-			var cacheMsg Message
-			if err := json.Unmarshal([]byte(msg.Payload), &cacheMsg); err != nil {
-				log.Printf("Error unmarshalling cache message: %s", err)
-				continue
-			}
+			cacheMsg := toMessage(msg.Payload)
 
 			if cacheMsg.SenderID == c.id {
 				continue
@@ -39,11 +34,18 @@ func (c *Cache) Invalidate(ctx context.Context, key string) error {
 		SenderID: c.id,
 		Key:      key,
 	}
+	return c.redis.Publish(ctx, c.channel, msg.String()).Err()
+}
 
-	data, err := json.Marshal(msg)
-	if err != nil {
-		return err
+func (m Message) String() string {
+	return fmt.Sprintf("%s %s", m.SenderID, m.Key)
+}
+
+func toMessage(keyString string) Message {
+	senderId := keyString[:16]
+	key := keyString[17:]
+	return Message{
+		SenderID: senderId,
+		Key:      key,
 	}
-
-	return c.redis.Publish(ctx, c.channel, data).Err()
 }
